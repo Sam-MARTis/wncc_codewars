10d8cf952b84489937d62721d49c508de31dc977
Sam-MARTis <samanthmartis@gmail.com>
2024-03-17 18:39:15 +0530

    resource prioritized:

---
+++ codewars-v4/scriptblue.py
@@ -0,0 +1,101 @@
+import random
+import math
+# import threading
+
+name = "scriptblue"
+
+
+def moveTo(x, y, Pirate):
+    position = Pirate.getPosition()
+    if position[0] == x and position[1] == y:
+        return 0
+    if position[0] == x:
+        return (position[1] < y) * 2 + 1
+    if position[1] == y:
+        return (position[0] > x) * 2 + 2
+    if random.randint(1, 2) == 1:
+        return (position[0] > x) * 2 + 2
+    else:
+        return (position[1] < y) * 2 + 1
+
+
+def ActPirate(pirate):
+    try:
+        if pirate.randX == None:
+            pirate.randX = 40//10 * random.randint(0,10)
+    except:
+        pirate.randX = 40//10 * random.randint(0,10)
+    try:
+        if pirate.randY == None:
+            pirate.randY = 40//5 * random.randint(0,10)
+    except:
+        pirate.randY = 40//10 * random.randint(0,10)
+    up = pirate.investigate_up()[0]
+    down = pirate.investigate_down()[0]
+    left = pirate.investigate_left()[0]
+    right = pirate.investigate_right()[0]
+    x, y = pirate.getPosition()
+    pirate.setSignal("")
+    s = pirate.trackPlayers()
+    
+    if (
+        (up == "island1" and s[0] != "myCaptured")
+        or (up == "island2" and s[1] != "myCaptured")
+        or (up == "island3" and s[2] != "myCaptured")
+    ):
+        s = up[-1] + str(x) + "," + str(y - 1)
+        pirate.setTeamSignal(s)
+
+    if (
+        (down == "island1" and s[0] != "myCaptured")
+        or (down == "island2" and s[1] != "myCaptured")
+        or (down == "island3" and s[2] != "myCaptured")
+    ):
+        s = down[-1] + str(x) + "," + str(y + 1)
+        pirate.setTeamSignal(s)
+
+    if (
+        (left == "island1" and s[0] != "myCaptured")
+        or (left == "island2" and s[1] != "myCaptured")
+        or (left == "island3" and s[2] != "myCaptured")
+    ):
+        s = left[-1] + str(x - 1) + "," + str(y)
+        pirate.setTeamSignal(s)
+
+    if (
+        (right == "island1" and s[0] != "myCaptured")
+        or (right == "island2" and s[1] != "myCaptured")
+        or (right == "island3" and s[2] != "myCaptured")
+    ):
+        s = right[-1] + str(x + 1) + "," + str(y)
+        pirate.setTeamSignal(s)
+
+    
+    if pirate.getTeamSignal() != "":
+        s = pirate.getTeamSignal()
+        l = s.split(",")
+        x = int(l[0][1:])
+        y = int(l[1])
+    
+        # return moveTo(x, y, pirate)
+        return moveTo(pirate.randX + random.randint(0,4), pirate.randY+ random.randint(0,3), pirate)
+
+    else:
+        # return random.randint(1, 4)
+        return moveTo(pirate.randX + random.randint(0,4), pirate.randY+ random.randint(0,3), pirate)
+
+
+def ActTeam(team):
+    l = team.trackPlayers()
+    s = team.getTeamSignal()
+
+    team.buildWalls(1)
+    team.buildWalls(2)
+    team.buildWalls(3)
+    # print(team.getTeamSignal())
+    # print(team.trackPlayers())
+    if s:
+        island_no = int(s[0])
+        signal = l[island_no - 1]
+        if signal == "myCaptured":
+            team.setTeamSignal("")
