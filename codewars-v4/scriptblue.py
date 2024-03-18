ea6501f371330ce7a6633a48079a1708a89fafb4
Sam-MARTis <samanthmartis@gmail.com>
2024-03-18 16:08:21 +0530

    fix: fixed island capture and then leaving

--- codewars-v4/scriptblue.py
+++ codewars-v4/scriptblue.py
@@ -1,6 +1,6 @@
 import random
 import math
-# import threading
+
 
 name = "scriptblue"
 units = 10
@@ -39,54 +39,22 @@ def ActPirate(pirate):
     x, y = pirate.getPosition()
     pirate.setSignal("")
     s = pirate.trackPlayers()
-    try:
-        if pirate.obey==0:
-            return
-    except:
-        pirate.obey=1
+
     
-    if (
-        (up == "island1" and s[0] != "myCaptured")
-        or (up == "island2" and s[1] != "myCaptured")
-        or (up == "island3" and s[2] != "myCaptured")
-    ):
-        # s = up[-1] + str(x) + "," + str(y - 1)
-        # pirate.setTeamSignal(s)
-        pirate.obey=0
+    if(any([up==f"island{i+1}" and s[i]!="myCaptured" for i in range(3)])):
+        # pirate.obey=0
         return moveTo(x, y-1, pirate)
-        
-
-    if (
-        (down == "island1" and s[0] != "myCaptured")
-        or (down == "island2" and s[1] != "myCaptured")
-        or (down == "island3" and s[2] != "myCaptured")
-    ):
-        # s = down[-1] + str(x) + "," + str(y + 1)
-        # # pirate.setTeamSignal(s)
-        # return moveTo
-        pirate.obey=0
+    if(any([down==f"island{i+1}" and s[i]!="myCaptured" for i in range(3)])):
+        # pirate.obey=0
         return moveTo(x, y+1, pirate)
-
-    if (
-        (left == "island1" and s[0] != "myCaptured")
-        or (left == "island2" and s[1] != "myCaptured")
-        or (left == "island3" and s[2] != "myCaptured")
-    ):
-        # s = left[-1] + str(x - 1) + "," + str(y)
-        # pirate.setTeamSignal(s)
-        pirate.obey=0
-        return moveTo(x-1, y, pirate)
-
-    if (
-        (right == "island1" and s[0] != "myCaptured")
-        or (right == "island2" and s[1] != "myCaptured")
-        or (right == "island3" and s[2] != "myCaptured")
-    ):
-        # s = right[-1] + str(x + 1) + "," + str(y)
-        # pirate.setTeamSignal(s)
-        pirate.obey=0
+    if(any([right==f"island{i+1}" and s[i]!="myCaptured" for i in range(3)])):
+        # pirate.obey=0
+        return moveTo(x-1, y-1, pirate)
+    if(any([left==f"island{i+1}" and s[i]!="myCaptured" for i in range(3)])):
+        # pirate.obey=0
         return moveTo(x+1, y, pirate)
-    pirate.obey=1
+        
+ 
     global game_clock
     if game_clock%50 ==0:
         pirate.randX = 40//units * random.randint(0,units-1)
@@ -98,11 +66,10 @@ def ActPirate(pirate):
         x = int(l[0][1:])
         y = int(l[1])
     
-        # return moveTo(x, y, pirate)
+
         return moveTo(pirate.randX + random.randint(0,4), pirate.randY+ random.randint(0,4), pirate)
 
     else:
-        # return random.randint(1, 4)
         return moveTo(pirate.randX + random.randint(0,4), pirate.randY+ random.randint(0,4), pirate)
 
 
@@ -115,8 +82,7 @@ def ActTeam(team):
     team.buildWalls(2)
     team.buildWalls(3)
     
-    # print(f"Team signal is: {team.getTeamSignal()}\n")
-    # print(f"Track players: {team.trackPlayers()}\n")
+ 
     game_clock+=1
     if game_clock%50==0:
         print("Changing team signal")
